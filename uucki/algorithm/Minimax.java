c9cd8a1d399ba174f04575f825a88920ec0e5a6f
richard@xeli.eu <richard@xelification.com>
2016-08-06 23:48:36 +0200

    fix a bug where the heurisic for the wrong color was being used + return the moves the minmax is predicting that will be played + implement iterative depth first, rather than a static depth first

--- uucki/algorithm/Minimax.java
+++ uucki/algorithm/Minimax.java
@@ -12,49 +12,70 @@ import java.util.*;
 public class Minimax extends Algorithm {
 
     public Move run(Board board, FieldValue color) {
-
-        List<Position> possiblePositions = PossibleMoves.getValidPositions(board, color);
-        System.out.println(possiblePositions.size());
-        double value = Double.NEGATIVE_INFINITY;
-        Move resultMove = null;
-        for(Position position : possiblePositions) {
-            Move move = new Move(position, color);
-            double newValue = minValue(5, board.makeMove(move), otherPlayer(color));
-            if(newValue > value) {
-                resultMove = move;
-                value = newValue;
+        long startingTime = System.currentTimeMillis();
+        Move bestMove = null;
+        int depth = 1;
+        long cutOffTime = startingTime + 1000 * 5;
+        System.out.println(board.emptyFields());
+        while((System.currentTimeMillis() < cutOffTime || bestMove == null) && depth <= board.emptyFields()) {
+            Result result = maxValue(depth++, board, color, cutOffTime);
+            System.out.println(Arrays.toString(result.moves.toArray()));
+            Move move = result.moves.get(result.moves.size() - 1);
+            System.out.println(move);
+            System.out.println("Depth: " + depth);
+            if(System.currentTimeMillis() < cutOffTime) {
+                bestMove = move;
             }
         }
-
-        return resultMove;
+        return bestMove;
     }
 
-    public double minValue(int depth, Board board, FieldValue color) {
-        if(depth == 0) {
-            return Basic.getValue(board, color);
+    public Result minValue(int depth, Board board, FieldValue color, long cutOffTime) {
+        if(depth == 0 || System.currentTimeMillis() > cutOffTime) {
+            return new Result((int)Basic.getValue(board,otherPlayer(color)));
         }
-        List<Position> possiblePositions = PossibleMoves.getValidPositions(board, color);
-        double value = Double.POSITIVE_INFINITY;
+
+        if(board.isFinished()) {
+            return new Result(board.getWinner() == otherPlayer(color) ? 1000 : -1000);
+        }
+
+        List<Position> possiblePositions = PossibleMoves.getValidPositions(board,color);
+
+        //if there are no possible moves, the opponent can go again
+        if(possiblePositions.size() == 0) {
+            return maxValue(depth, board, otherPlayer(color), cutOffTime);
+        }
+
+        Result result = null;
 
         for(Position position : possiblePositions) {
             Move move = new Move(position, color);
-            value = Math.min(value, maxValue(depth-1, board.makeMove(move), otherPlayer(color)));
+            result = newResultMin(result, maxValue(depth-1, board.makeMove(move), otherPlayer(color), cutOffTime).add(move));
         }
-        return value;
+        return result;
     }
 
-    public double maxValue(int depth, Board board, FieldValue color) {
-        if(depth == 0) {
-            return Basic.getValue(board, color);
+    public Result maxValue(int depth, Board board, FieldValue color, long cutOffTime) {
+        if(depth == 0 || System.currentTimeMillis() > cutOffTime) {
+            return new Result((int)Basic.getValue(board,color));
+        }
+
+        if(board.isFinished()) {
+            return new Result(board.getWinner() == color ? 1000 : -1000);
         }
         List<Position> possiblePositions = PossibleMoves.getValidPositions(board, color);
-        double value = Double.NEGATIVE_INFINITY;
+
+        //if there are no possible moves, the opponent can go again
+        if(possiblePositions.size() == 0) {
+            return minValue(depth, board, otherPlayer(color), cutOffTime);
+        }
+        Result result = null;
 
         for(Position position : possiblePositions) {
             Move move = new Move(position, color);
-            value = Math.max(value, minValue(depth-1, board.makeMove(move), otherPlayer(color)));
+            result = newResultMax(result, minValue(depth-1, board.makeMove(move), otherPlayer(color), cutOffTime).add(move));
         }
-        return value;
+        return result;
     }
 
     private FieldValue otherPlayer(FieldValue color) {
@@ -65,4 +86,33 @@ public class Minimax extends Algorithm {
         return FieldValue.WHITE;
     }
 
+    private Result newResultMin(Result result, Result newResult) {
+        if(result == null || newResult.score < result.score) {
+            return newResult;
+        }
+
+        return result;
+    }
+    private Result newResultMax(Result result, Result newResult) {
+        if(result == null || newResult.score > result.score) {
+            return newResult;
+        }
+
+        return result;
+    }
+
+}
+
+class Result {
+    List<Move> moves = new ArrayList<Move>();
+    int score = 0;
+
+    public Result(int score) {
+        this.score = score;
+    }
+
+    public Result add(Move move) {
+        moves.add(move);
+        return this;
+    }
 }
