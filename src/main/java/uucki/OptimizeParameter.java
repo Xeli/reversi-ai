a6bb4832088e4ba4382bd2fe891a66fd025bbc80
richard@xeli.eu <richard@xelification.com>
2017-03-31 19:42:16 +0200

    tweak the evolutionary algo a bit to find c values faster

--- src/main/java/uucki/OptimizeParameter.java
+++ src/main/java/uucki/OptimizeParameter.java
@@ -14,21 +14,21 @@ import java.util.*;
 
 public class OptimizeParameter {
 
-    public final static int POPULATION_LIMIT = 4;
-    public final static int GENERATIONS = 5;
+    public final static int POPULATION_LIMIT = 1;
+    public final static int GENERATIONS = 1;
     public static Random random;
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         random = new Random();
 
-        int lowEnd = 0;
-        int highEnd = 10;
+        double lowEnd = 0.0;
+        double highEnd = 10.0;
         try {
             System.out.println("Low end number to search");
-            lowEnd = Integer.parseInt(br.readLine());
+            lowEnd = Double.parseDouble(br.readLine());
             System.out.println("high end number to search");
-            highEnd = Integer.parseInt(br.readLine());
+            highEnd = Double.parseDouble(br.readLine());
         } catch (IOException e) {
             System.out.println("IO error");
             return;
@@ -42,30 +42,39 @@ public class OptimizeParameter {
         }
 
         for(int i = 0; i < GENERATIONS; i++) {
-            population = grow(population, i, lowEnd, highEnd);
+            double mean = mean(population);
+            double variance = variance(population);
+            System.out.println(i + "," + mean + "," + variance);
+            population = grow(population, variance);
             population = tournament(population);
         }
 
     }
 
-    public static List<Double> grow(List<Double> population, int generation, double low, double high) {
+    private static double mean(List<Double> list) {
+        return list.stream().mapToDouble(a -> a).average().getAsDouble();
+    }
+
+    private static double variance(List<Double> list) {
+        double mean = mean(list);
+        return list.stream().mapToDouble(a -> Math.abs(mean - a)).sum() / list.size();
+    }
+
+    public static List<Double> grow(List<Double> population, double variance) {
         List<Double> newPopulation = new ArrayList<Double>(population);
-        population.stream().map(d -> mutate(d, generation, low, high)).forEach(d -> newPopulation.add(d));
-        System.out.println(population);
-        System.out.println(newPopulation);
+        population.stream().map(d -> mutate(d, variance)).forEach(d -> newPopulation.add(d));
         return newPopulation;
     }
 
-    private static double mutate(double value, int generation, double low, double high) {
-        double range = 0.3 - Math.pow(0.3 * (generation / (double)GENERATIONS), 3.0);
-        return random.nextGaussian() * (high - low) * range + value;
+    private static double mutate(double value, double variance) {
+        double range = variance;
+        return random.nextGaussian() * range + value;
     }
 
     public static List<Double> tournament(List<Double> population) {
         List<Double> newPopulation = new ArrayList<Double>(POPULATION_LIMIT);
 
         while(population.size() >= 2) {
-            System.out.println(population.size());
             Double value1 = anyItem(population);
             Double value2 = anyItem(population);
             newPopulation.add(runGame(value1, value2));
@@ -81,10 +90,10 @@ public class OptimizeParameter {
     }
 
     public static double runGame(double value1, double value2) {
-        System.out.println("Running game, white: " + value1 + ", black: " + value2);
+        //System.out.println("Running game, white: " + value1 + ", black: " + value2);
         int value1Wins = 0;
         int value2Wins = 0;
-        for(int i = 0; i < 4; i++) {
+        for(int i = 0; i < 2; i++) {
             double winningValue = 0;
             if (i % 2 == 0) {
                 winningValue = runSingleGame(value1, value2);
@@ -97,7 +106,7 @@ public class OptimizeParameter {
                 value2Wins++;
             }
         }
-        System.out.println("Winner: " + (value1Wins > value2Wins ? value1 : value2));
+        //System.out.println("Winner: " + (value1Wins > value2Wins ? value1 : value2));
         if(value1Wins > value2Wins) {
             return value1;
         }
@@ -105,8 +114,8 @@ public class OptimizeParameter {
     }
 
     public static double runSingleGame(double value1, double value2) {
-        Algorithm ai1 = new MonteCarloTreeSearch(value1);
-        Algorithm ai2 = new MonteCarloTreeSearch(value2);
+        Algorithm ai1 = new MonteCarloTreeSearch(value1, true, false);
+        Algorithm ai2 = new MonteCarloTreeSearch(value2, true, false);
         Board board = Board.initialBoard(false);
         AIvsAI mode = new AIvsAI(board, ai1, ai2);
         board = mode.game();
