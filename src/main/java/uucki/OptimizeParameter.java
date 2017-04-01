1642981f67e780f9a9c7ea19683bd618f35f455d
richard@xeli.eu <richard@xelification.com>
2017-04-01 10:20:37 +0200

    add method to optimizeparameter to see test a fixed population, and rank them by wins

--- src/main/java/uucki/OptimizeParameter.java
+++ src/main/java/uucki/OptimizeParameter.java
@@ -22,6 +22,40 @@ public class OptimizeParameter {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         random = new Random();
 
+        List<Double> population = new ArrayList<Double>();
+        population.add(0.25);
+        population.add(0.75);
+        population.add(1.0);
+        population.add(1.25);
+        population.add(1.5);
+        population.add(1.75);
+        population.add(2.0);
+        rankPopulation(population);
+    }
+
+    private static void rankPopulation(List<Double> population) {
+        HashMap<Double, Integer> score = new HashMap<Double, Integer>(population.size());
+        for(Double c : population) {
+            for(int round = 0; round < 10; round++) {
+
+                for(Double opponent : population) {
+                    if (opponent == c) {
+                        continue;
+                    }
+
+                    double winner = runGame(c, opponent);
+                    int currentScore = score.getOrDefault(winner, 0);
+                    score.put(winner, currentScore+1);
+                }
+            }
+        }
+
+        for(Map.Entry<Double, Integer> entry : score.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    private static void findBestC(BufferedReader br) {
         double lowEnd = 0.0;
         double highEnd = 10.0;
         try {
@@ -48,7 +82,6 @@ public class OptimizeParameter {
             population = grow(population, variance);
             population = tournament(population);
         }
-
     }
 
     private static double mean(List<Double> list) {
